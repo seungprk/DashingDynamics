8e2d9113e3c45426b0a7650104c880aaad906a41
mtso <mtso@tuta.io>
2016-12-14 15:38:36 -0800

    Comment out field for now

--- DashingPenguin/DashingPenguin/Scenes/GameSceneStatePlaying.swift
+++ DashingPenguin/DashingPenguin/Scenes/GameSceneStatePlaying.swift
@@ -21,13 +21,13 @@ class GameSceneStatePlaying: GKState {
     override func didEnter(from previousState: GKState?) {
         scene.view?.isPaused = false
         
-        let field = SKFieldNode.magneticField()
-        field.strength = 0.005
-        field.position = CGPoint.zero // scene.player!.component(ofType: SpriteComponent.self)!.node.position
-        field.region = SKRegion(size: CGSize(width: 3200, height: 3200))
-        field.categoryBitMask = GameplayConfiguration.PhysicsBitmask.field
-        field.isEnabled = true
-        scene.addChild(field)
+//        let field = SKFieldNode.magneticField()
+//        field.strength = 0.005
+//        field.position = CGPoint.zero // scene.player!.component(ofType: SpriteComponent.self)!.node.position
+//        field.region = SKRegion(size: CGSize(width: 3200, height: 3200))
+//        field.categoryBitMask = GameplayConfiguration.PhysicsBitmask.field
+//        field.isEnabled = true
+//        scene.addChild(field)
     }
     
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
