58eac2d3b5780628936a93709069bdd2ecf6b2f9
mtso <mtso@tuta.io>
2017-02-26 16:19:44 -0800

    Complete pause screen fix

--- DashingPenguin/DashingPenguin/Scenes/GameSceneStatePause.swift
+++ DashingPenguin/DashingPenguin/Scenes/GameSceneStatePause.swift
@@ -15,7 +15,6 @@ class GameSceneStatePause: GKState {
     
     unowned let scene : GameScene
     let pauseView: UIView
-    var unpBtn: UIButton?
     
     init(scene: GameScene) {
         self.scene = scene
@@ -23,8 +22,12 @@ class GameSceneStatePause: GKState {
         // Pause View
         pauseView = UIView(frame: scene.frame)
         pauseView.backgroundColor = .clear
-        pauseView.layer.transform = CATransform3DScale(CATransform3DIdentity, 2, 2, 1)
-//        pauseView.layer.transform = CATransform3DScale(CATransform3DIdentity, 2.5, 2.5, 1)
+        let scale = scene.view!.frame.width / 180
+        pauseView.layer.frame = CGRect(
+            x: scene.view!.frame.origin.x,
+            y: scene.view!.frame.origin.y,
+            width: scene.view!.frame.width / scale,
+            height: scene.view!.frame.height / scale)
         let viewX = scene.view!.frame.midX
         let viewY = scene.view!.frame.midY
         pauseView.layer.position.x = viewX
@@ -39,22 +42,24 @@ class GameSceneStatePause: GKState {
         
         // Unpause Button
         // Sprite image size: 41 x 21
-        let buttonSize = CGRect(x: 0, y: 0, width: 41 * 4, height: 21 * 4)
+        let buttonSize = CGRect(x: 0, y: 0, width: 41, height: 21)
         let unpauseButton = UIButton(frame: buttonSize)
-        let buttonX = viewX - pauseView.layer.frame.midX / 2
-        let buttonY = viewY - pauseView.layer.frame.midY / 2
-        unpauseButton.layer.position = CGPoint(x: buttonX, y: buttonY)
-        unpauseButton.setImage(UIImage(named: "unpause-button"), for: .normal)
+        unpauseButton.frame.origin = CGPoint(
+            x: pauseView.frame.size.width / 2 - unpauseButton.frame.size.width / 2,
+            y: pauseView.frame.size.height / 2 - unpauseButton.frame.size.height / 2)
+        let buttonImage = UIImage(named: "unpause-button")
+        unpauseButton.setImage(buttonImage, for: .normal)
         unpauseButton.imageView?.layer.magnificationFilter = kCAFilterNearest
         unpauseButton.addTarget(self, action: #selector(log(gestureRecognizer:)), for: .allEvents)
-        unpBtn = unpauseButton
         pauseView.addSubview(blurEffectView)
         pauseView.addSubview(unpauseButton)
+        
+        
+        pauseView.transform = CGAffineTransform(scaleX: scale, y: scale)
     }
     
     func log(gestureRecognizer: UIGestureRecognizer) {
         scene.stateMachine.enter(GameSceneStatePlaying.self)
-        print("UNPAUSE BUTTON \(unpBtn!.layer.position)")
     }
     
     override func didEnter(from previousState: GKState?) {
