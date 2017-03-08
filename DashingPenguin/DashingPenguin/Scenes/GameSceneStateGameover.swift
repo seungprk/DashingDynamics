d8f3bff5ad372026c3a0bfbda56847d388b00f5a
mtso <mtso@tuta.io>
2017-03-08 11:20:50 -0800

    Add notes to gameover

--- DashingPenguin/DashingPenguin/Scenes/GameSceneStateGameover.swift
+++ DashingPenguin/DashingPenguin/Scenes/GameSceneStateGameover.swift
@@ -21,6 +21,19 @@ class GameSceneStateGameover: GKState {
     override func didEnter(from previousState: GKState?) {
         print("game over")
         
+        // Once we enter this scene
+        // we need to animate in the shell border
+        // calculate the session statistics
+        // display the session statistics
+        //   high score
+        //   current score
+        //   distance traveled
+        //   points collected
+        //     NOTE: make sure the stats are formatted, padded, and aligned properly
+        // add button interaction to 
+        //   exit to menu
+        //   restart a new session
+        
         let delay = SKAction.wait(forDuration: 2.0)
         
         scene.run(delay) {
