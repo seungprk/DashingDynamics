327a132604a8c61758ee3affb7e3f9b0dc2e3fd6
mtso <mtso@tuta.io>
2017-03-12 17:20:02 -0700

    Begin work on tile transition for gameover

--- DashingPenguin/DashingPenguin/Scenes/GameSceneStateGameover.swift
+++ DashingPenguin/DashingPenguin/Scenes/GameSceneStateGameover.swift
@@ -11,11 +11,16 @@ import GameplayKit
 
 class GameSceneStateGameover: GKState {
     
+    // Reference scene
     unowned let scene : GameScene
+    
+    // Re-used tile texture
+    let tileTexture = SKTexture(imageNamed: "deathtile")
     
     init(scene: GameScene) {
         self.scene = scene
         super.init()
+        tileTexture.filteringMode = .nearest
     }
     
     override func didEnter(from previousState: GKState?) {
@@ -25,12 +30,18 @@ class GameSceneStateGameover: GKState {
         // we need to animate in the shell border
         // fade the game scene to black.
         
-        // calculate the session statistics
-        guard let scoreManager = scene.scoreManager else { return }
-        let totalScore    = scoreManager.getTotalScore()
-        let distanceScore = scoreManager.getDistance()
-        let platformScore = scoreManager.getPlatformScore()
+        // Calculate the session statistics
+//        guard let scoreManager = scene.scoreManager else { return }
+//        let totalScore    = scoreManager.getTotalScore()
+//        let distanceScore = scoreManager.getDistance()
+//        let platformScore = scoreManager.getPlatformScore()
 
+        
+        deathTransition() {
+            let againButton = self.makeAgainButton()
+            self.scene.addChild(againButton)
+        }
+        
         // display the session statistics
         //   high score
         //   current score
@@ -40,20 +51,63 @@ class GameSceneStateGameover: GKState {
         // add button interaction to 
         //   exit to menu
         //   restart a new session
-        
-        let delay = SKAction.wait(forDuration: 2.0)
-        
-        scene.run(delay) {
-            let transition = SKTransition.fade(withDuration: 1)
-            
-            if let view = self.scene.view,
-               let menu = self.scene.menuScene{
-                view.presentScene(menu, transition: transition)
-            }
-        }
+
     }
     
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         return stateClass is GameSceneStateSetup.Type
     }
+    
+    private func makeAgainButton() -> SKButton {
+        let againButton = SKButton(
+            nameForImageNormal: "again-button",
+            nameForImageNormalHighlight: nil
+        )
+        againButton.name = "again_button"
+        againButton.delegate = self
+        againButton.zPosition = 1000000000 // TODO: set this to a reasonable zPosition based on config
+        againButton.position = CGPoint(
+            x: scene.frame.midX - againButton.frame.width / 2,
+            y: scene.frame.midY - againButton.frame.height / 2
+        )
+        return againButton
+    }
+
+    // Tile Transition Methods
+    
+    private func deathTransition(completion: @escaping () -> Void) {
+        
+        let tile = SKSpriteNode(texture: tileTexture)
+        tile.position = CGPoint(x: scene.frame.midX, y: scene.frame.midY)
+        tile.zPosition = 1000000000
+        scene.addChild(tile)
+        growTile(tile) {
+            completion()
+        }
+        
+        // animate death tiles
+    }
+    
+    private func growTile(_ tile: SKSpriteNode, completion: @escaping () -> Void) {
+        tile.scale(to: CGSize.zero)
+        let scaleSpeed = 4.0
+        let scaleToFull = SKAction.scale(to: 1, duration: scaleSpeed)
+        tile.run(scaleToFull) {
+            completion()
+        }
+    }
 }
+
+extension GameSceneStateGameover: SKButtonDelegate {
+    func onButtonPress(named: String) {
+        guard named == "again_button" else { return }
+        
+        let transition = SKTransition.fade(withDuration: 1)
+        if let view = self.scene.view,
+            let menu = self.scene.menuScene{
+            view.presentScene(menu, transition: transition)
+        }
+    }
+}
+
+
