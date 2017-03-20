c8af3ad053947becb33f2f9acc9f8fbed473ab4c
mtso <mtso@tuta.io>
2017-03-19 22:26:27 -0700

    Complete score label

--- DashingPenguin/DashingPenguin/Node/SKScoreLabel.swift
+++ DashingPenguin/DashingPenguin/Node/SKScoreLabel.swift
@@ -10,26 +10,78 @@ import SpriteKit
 
 class SKScoreLabel: SKNode {
     
-    var numberFont = [SKTexture]()
+    var digits = [SKSpriteNode]()
+    let fontsize: CGSize
+    let numberFont = SKTextureAtlas(named: "number-font").textures()
     
-    override init() {
-        super.init()
-        initNumberFont()
-        let test = SKSpriteNode(texture: numberFont.first)
-        self.addChild(test)
+    /// The size containing digit sprites.
+    var size: CGSize {
+        get {
+            return CGSize(
+                width: fontsize.width * CGFloat(digits.count),
+                height: fontsize.height
+            )
+        }
     }
     
-    private func initNumberFont() {
-        let atlas = SKTextureAtlas(named: "number-font")
-        atlas.textureNames.forEach { name in
-            let texture = atlas.textureNamed(name)
-            texture.filteringMode = .nearest
-            numberFont.append(texture)
-        }
+    override init() {
+        fontsize = numberFont.first!.size()
+        super.init()
+        
+    #if DEBUG
+        let center = SKSpriteNode(color: .red, size: CGSize(width: 1, height: 1))
+        center.zPosition = 300000000000000000 // just needs to be above anything
+        addChild(center)
+    #endif
+    }
+    
+    convenience init(value: Int) {
+        self.init()
+        self.setValue(to: value)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func setValue(to number: Int) {
+        self.removeDigitNodes()
+        self.digits = number.digits()
+                            .enumerated()
+                            .map(toSprite)
+    }
+    
+    private func removeDigitNodes() {
+        for _ in 0..<self.digits.count {
+            self.digits.popLast()?.removeFromParent()
+        }
+    }
+    
+    private func toSprite(at index: Int, for digit: Int) -> SKSpriteNode {
+        let node = SKSpriteNode(texture: self.numberFont[digit])
+        let offset = CGFloat(index) - (CGFloat(digits.count - 1) / 2)
+        node.position.x = offset * self.fontsize.width
+        self.addChild(node)
+        return node
+    }
 }
 
+extension SKTextureAtlas {
+    /// Creates an array of textures from the data stored in the atlas object.
+    func textures() -> [SKTexture] {
+        return self.textureNames.sorted().map({ name in
+            return self.textureNamed(name)
+        })
+    }
+}
+
+extension Int {
+    /// Converts an integer into an array of its digits.
+    func digits() -> [Int] {
+        return String(self).characters.map({ character in
+            let charString = String(character)
+            // Convert string to integer
+            return Int(charString, radix: 10)!
+        })
+    }
+}
