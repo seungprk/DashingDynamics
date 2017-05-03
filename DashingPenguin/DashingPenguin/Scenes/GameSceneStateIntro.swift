b360317815ce1543fd54cca96681f12eae8635be
seungprk@gmail.com <seungprk@gmail.com>
2017-05-02 21:48:29 -0700

    tweak starting drop animation

--- DashingPenguin/DashingPenguin/Scenes/GameSceneStateIntro.swift
+++ DashingPenguin/DashingPenguin/Scenes/GameSceneStateIntro.swift
@@ -24,14 +24,19 @@ class GameSceneStateIntro: GKState {
         // Setup textures
         let playerAtlas = (scene.player?.playerAnimatedAtlas)!
         var playerTextureFrames = [SKTexture]()
-        playerTextureFrames.append(playerAtlas.textureNamed("playerdash-1"))
-        playerTextureFrames.append(playerAtlas.textureNamed("playerdash45-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash90-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash135-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash180-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash225-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash270-1"))
         playerTextureFrames.append(playerAtlas.textureNamed("playerdash315-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash45-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash45-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash90-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash90-1"))
+        playerTextureFrames.append(playerAtlas.textureNamed("playerdash90-1"))
         for texture in playerTextureFrames {
             texture.filteringMode = .nearest
         }
@@ -46,12 +51,11 @@ class GameSceneStateIntro: GKState {
         let playerLandPos = CGPoint(x: 0, y: GameplayConfiguration.Platform.size.height / 2)
         
         let dropAction = SKAction.move(to: playerLandPos, duration: 1.5)
+        dropAction.timingMode = SKActionTimingMode.easeOut
         let spinAction = SKAction.animate(with: playerTextureFrames, timePerFrame: 0.1)
-        let spinRepeat = SKAction.repeatForever(spinAction)
         
-        playerSprite?.run(spinRepeat, withKey: "spinRepeat")
+        playerSprite?.run(spinAction)
         playerSprite?.run(dropAction, completion: {
-            playerSprite?.removeAction(forKey: "spinRepeat")
             self.stateMachine?.enter(GameSceneStatePlaying.self)
         })
     }
