e99bf7e6a7105036fc86830693494af02b27d31b
seungprk@gmail.com <seungprk@gmail.com>
2017-05-03 00:02:11 -0700

    add afterimage effect to dashing

--- DashingPenguin/DashingPenguin/Components/DashingState.swift
+++ DashingPenguin/DashingPenguin/Components/DashingState.swift
@@ -25,7 +25,7 @@ class DashingState: GKState, WallContactDelegate {
         // Remove DashEndingState's visual effects
         entity.component(ofType: MovementComponent.self)?.dashCount += 1
         if let spriteComponent = self.entity.component(ofType: SpriteComponent.self) {
-            spriteComponent.node.removeAction(forKey: "flashingSequence")
+            spriteComponent.node.removeAllActions()
             spriteComponent.node.alpha = 1
         }
         
@@ -39,6 +39,39 @@ class DashingState: GKState, WallContactDelegate {
         }
         
         AudioManager.sharedInstance.play("energy-burst")
+        
+        // Trailing Effect
+        var prevTime: CGFloat = 0.0
+        var accumTime: CGFloat = 0.0
+        
+        if let playerNode = entity.component(ofType: SpriteComponent.self)?.node {
+            let gameScene = playerNode.scene as! GameScene
+            
+            let trailAction = SKAction.customAction(withDuration: GameplayConfiguration.Player.dashDuration) {
+                node, elapsedTime in
+                
+                let deltaTime = elapsedTime - prevTime
+                accumTime += deltaTime
+                prevTime = elapsedTime
+                
+                if elapsedTime == 0.0 || accumTime > 0.05 {
+                    accumTime = 0.0
+                    if let node = node as? SKSpriteNode {
+                        
+                        let trailNode = SKSpriteNode(texture: playerNode.texture)
+                        trailNode.position = node.position
+                        trailNode.zPosition = node.zPosition - 0.0001
+                        trailNode.alpha = 0.5
+                        gameScene.sceneEffectNode.addChild(trailNode)
+                        
+                        let delayRemoveSequence = SKAction.sequence([SKAction.wait(forDuration: 0.2), SKAction.removeFromParent()])
+                        trailNode.run(delayRemoveSequence)
+                    }
+                }
+            }
+            
+            playerNode.run(trailAction)
+        }
     }
     
     override func update(deltaTime seconds: TimeInterval) {
